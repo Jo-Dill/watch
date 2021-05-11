e0f8d55ccfb4493f077984b4b6f2f70b2dcf2d55
Jo-Dill <84033115+Jo-Dill@users.noreply.github.com>
2021-05-11 18:24:17 +0200

    Delete WristWatch1.3 directory

--- WristWatch1.3/Neopixel.ino
+++
@@ -1,5 +0,0 @@
-#define NUMPIXELS 12
-Adafruit_NeoPixel pixels(NUMPIXELS, leddataPin, NEO_GRB + NEO_KHZ800);
-void neopixelSetup (){
-  pixels.begin();
-}
--- WristWatch1.3/timerset.ino
+++
@@ -1,5 +0,0 @@
-void timerSet (){
-  pixels.clear();
-  pixels.setPixelColor(0, pixels.Color(0,0,(5*brightness)));
-  pixels.show();
-}
